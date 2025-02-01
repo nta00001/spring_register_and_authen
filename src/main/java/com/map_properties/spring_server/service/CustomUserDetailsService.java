84456a6cd82bc0184b861348da9a719b7f96b2b4
Chí Công <cong.pc2305@gmail.com>
2025-02-02 01:57:46 +0700

    remove - useless

--- src/main/java/com/map_properties/spring_server/service/CustomUserDetailsService.java
+++ src/main/java/com/map_properties/spring_server/service/CustomUserDetailsService.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.context.annotation.Lazy;
 
 import com.map_properties.spring_server.entity.User;
 import com.map_properties.spring_server.repository.UserRepository;
@@ -16,10 +14,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     UserRepository userRepository;
-
-    @Lazy
-    @Autowired
-    private PasswordEncoder encoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) {
