9e38348844d3e6ab2953ed3e015cf24b9a8e086b
Chí Công <cong.pc2305@gmail.com>
2025-02-02 01:30:46 +0700

    update

--- src/main/java/com/map_properties/spring_server/exception/LoginException.java
+++ src/main/java/com/map_properties/spring_server/exception/LoginException.java
@@ -1,6 +1,6 @@
 package com.map_properties.spring_server.exception;
 
-public class LoginException extends Exception {
+public class LoginException extends RuntimeException {
     public LoginException(String message) {
         super(message);
     }
